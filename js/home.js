31b29d2997645c5c1098a6376bd57309af151b7d
Joel Teyber <jteyber@dotscreen.com>
2015-10-30 10:42:09 +0100

    new home ! enfin avec 4 boutons encore

--- js/home.js
+++ js/home.js
@@ -6,21 +6,35 @@ function homeScreen() {
 
 	this.init = function() {
 		if(!this.alreadyInit) {
-			var menuButton = createButton("homeVideo", myHomeScreen.homeScreen, "homeMenu", 0, 0, "menuButton");
-			menuButton.style.margin = "0px 80px 80px 0px";
+			var menuButton = createButton("homeBabdp", myHomeScreen.homeScreen, "homeMenu", 0, 0, "menuButton homeBabdp");
+			menuButton.style.margin = "0px 25px 25px 0px";
 			menuButton.setAttribute("tabindex", 5);
-			createImg(null, menuButton, "media/home/home_video.png", null, "Section Video");
-			menuButton = createButton("homeMusic", myHomeScreen.homeScreen, "homeMenu", 1, 0, "menuButton");
-			menuButton.style.margin = "0px 0px 80px 0px";
+			createImg(null, menuButton, "media/home/baddp.png", null, "Appplication BABDP");
+			menuButton = createButton("homePluzz", myHomeScreen.homeScreen, "homeMenu", 1, 0, "menuButton homePluzz");
+			menuButton.style.margin = "0px 0px 25px 0px";
 			menuButton.setAttribute("tabindex", 6);
-			createImg(null, menuButton, "media/home/home_musique.png", null, "Section Musique");
-			menuButton = createButton("homeService", myHomeScreen.homeScreen, "homeMenu", 0, 1, "menuButton");
-			menuButton.style.margin = "0px 80px 0px 0px";
+			createImg(null, menuButton, "media/home/pluzz.png", null, "Appplication Pluzz");
+/*
+			menuButton = createButton("homePlayVideoTop", myHomeScreen.homeScreen, "homeMenuVideo", 1, 0, "doubleMenuButton");
+			menuButton.style.margin = "0px 0px 25px 25px";
+			menuButton.setAttribute("tabindex", 6);
+			createImg(null, menuButton, "", null, "Lecture de la vidéo : Human");
+
+			menuButton = createButton("homePlayVideoBottom", myHomeScreen.homeScreen, "homeMenuVideo", 1, 0, "doubleMenuButton");
+			menuButton.style.margin = "0px 25px 25px 0px";
+			menuButton.setAttribute("tabindex", 6);
+			createImg(null, menuButton, "", null, "Lecture de la vidéo : La chouette et la compagnie");
+*/
+			menuButton = createButton("homeLeSite", myHomeScreen.homeScreen, "homeMenu", 0, 1, "menuButton");
+			menuButton.style.margin = "425px 0px 0px -825px";
 			menuButton.setAttribute("tabindex", 7);
-			createImg(null, menuButton, "media/home/home_service.png", null, "Section Service");
-			menuButton = createButton("homePoste", myHomeScreen.homeScreen, "homeMenu", 1, 1, "menuButton");
+			createImg(null, menuButton, "media/home/lesitetv.png", null, "Section lesite.tv");
+			menuButton = createButton("homeMediatheque", myHomeScreen.homeScreen, "homeMenu", 1, 1, "menuButton");
+			menuButton.style.margin = "425px 0px 0px -400px";
 			menuButton.setAttribute("tabindex", 8);
-			createImg(null, menuButton, "media/home/home_poste.png", null, "La Poste, plus près de chez vous");
+			createImg(null, menuButton, "media/home/media.png", null, "Médiathèque");
+
+			myHomeScreen.homeScreen.style.padding = "200px 0px 0px 500px";
 			
 			this.alreadyInit = true;
 		}
