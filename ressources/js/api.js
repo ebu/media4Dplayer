008e40c70ca88e8c9bd07436eaa3383a1359062d
jeugene <jeugene@dotscreen.com>
2016-06-29 12:52:58 +0200

    doit faire une requete pour chaque résultats pour récupérer les détails

--- ressources/js/api.js
+++ ressources/js/api.js
@@ -71,39 +71,24 @@ API.getFavorites = function(callback_function){
 		json.load({
 			url: Config.perfectMemoryWS + "medias?auth_token=" + User.tokens.auth_token + "&types=movie&max_count=10&offset=0&sort_fields=created_at&sort_order=-1",
 			callback: function(data) {
-				console.log(data);
 				
-				var i, list = [], count = 0, l = data.length;
-				for(i=0;i<l;i++){
-					
-					(function(media){
-						if(typeOf(media) === "object" && media.id){
-							var _onLoadTitle = function(title){
-								if(title){
-									media.title = title;
-									list.push(media);
-								}else{
-									log("L'item avec ce label n'a pas de titre : "+media.label);
-								}
+				// Génère une liste d'ID
+				var ids = [], x, dl = data.length;
+				for(x=0;x<dl;x++){
+					ids.push({idMovie:data[x].root_id});
+				}
 
-								count++;
-								if(count === l){
-									if(list.length){
-										Model.getResults(list, function(favorites){
-											callback_function({favorites:favorites});
-										});
+				// Charge les items les uns après les autres (la méthode when/then tombe en fail si l'une des requetes échoue)					
+				API.getItemsListForSearch(10, ids, function(list){
+					if(list.length){
+						Model.getResults(list, function(favorites){
+							callback_function({favorites:favorites});
+						});
 
-									}else{
-										callback_function();
-									}
-								}
-							};
-
-							// Fait une requete pour récuperer le titre du programme					
-							API.getProgramTitle(media.id, _onLoadTitle);
-						}
-					})(data[i]);
-				}				
+					}else{
+						callback_function();
+					}
+				});
 			},
 			headers: {
 				"Accept-language":"fr"
@@ -174,7 +159,7 @@ API.getResults = function(params, method, callback_function){
 					data = removeDuplicateItemInList(data, "idMovie");
 					
 					// Charge les items les uns après les autres (la méthode when/then tombe en fail si l'une des requetes échoue)					
-					API.getItemsListForSearch(method, data, function(list){
+					API.getItemsListForSearch(Config.limitResultForSearch, data, function(list){
 						if(list.length){
 							Model.getResults(list, callback_function);
 							
@@ -199,53 +184,53 @@ API.getResults = function(params, method, callback_function){
  * @param {Function} callback_function The function which will be triggered after receiving data
  */
 
-API.getItemsListForSearch = function(method, data, callback){
+API.getItemsListForSearch = function(limit, data, callback){
 		
 	var list = [];
-	if(method === "content"){
-		if(typeOf(data) === "array"){
-			
-			var l = data.length, count = 0, limit = Config.limitResultForSearch;
-			var _onLoadMediaData = function(jqXHR, textStatus){
-				if(textStatus === "success" && jqXHR.responseJSON){
-					
-					var data = jqXHR.responseJSON;
-					var _onLoadTitle = function(title){
-						if(title){
-							data.title = title;
-							list.push(data);
-							count++;
-							if(count === l || count === limit){
-								callback(list);
-							}
-						}
-					};
-					
-					// Fait une requete pour récuperer le titre du programme
-					API.getProgramTitle(data.id, _onLoadTitle);
-					
-				}else{
+	if(typeOf(data) === "array" && limit && typeOf(callback) === "function"){
+
+		var l = data.length, count = 0;
+		var _onLoadMediaData = function(jqXHR, textStatus){
+			if(textStatus === "success" && jqXHR.responseJSON){
+
+				var data = jqXHR.responseJSON;
+				var _onLoadTitle = function(title){
+					if(title){
+						data.title = title;
+						list.push(data);
+					}else{
+						log("L'item avec ce label n'a pas de titre : "+data.label);
+					}
 					count++;
 					if(count === l || count === limit){
 						callback(list);
 					}
+				};
+
+				// Fait une requete pour récuperer le titre du programme
+				API.getProgramTitle(data.id, _onLoadTitle);
+
+			}else{
+				count++;
+				if(count === l || count === limit){
+					callback(list);
 				}
-			};
-			
-			// Récupère les infos du programme
-			var i, media;
-			for(i=0;i<data.length&&i<limit;i++){
-				media = data[i];
-				if(typeOf(media) === "object" && media.idMovie){
-					$.ajax({
-						url: Config.perfectMemoryWS + "medias/root_id:"+media.idMovie+"?auth_token=" + User.tokens.auth_token,
-						complete:_onLoadMediaData,
-						timeout:Config.jsonTimeout * 1000,
-						headers: {
-							"Accept-language":"fr"
-						}
-					});
-				}
+			}
+		};
+
+		// Récupère les infos du programme
+		var i, media;
+		for(i=0;i<data.length&&i<limit;i++){
+			media = data[i];
+			if(typeOf(media) === "object" && media.idMovie){
+				$.ajax({
+					url: Config.perfectMemoryWS + "medias/root_id:"+media.idMovie+"?auth_token=" + User.tokens.auth_token,
+					complete:_onLoadMediaData,
+					timeout:Config.jsonTimeout * 1000,
+					headers: {
+						"Accept-language":"fr"
+					}
+				});
 			}
 		}
 	}
